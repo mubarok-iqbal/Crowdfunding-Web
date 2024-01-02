50331af746d6f65a28605fb723c8a67be73c3a94
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2024-01-02 22:05:12 +0700

    install tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,21 @@
 import { createApp } from 'vue'
-import Toast from "vue-toastification";
-
-import './style.css'
-import "vue-toastification/dist/index.css";
-
 import App from './App.vue'
 
+/**
+ * import Toastr
+ */
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+/**
+ * Tailwind CSS
+*/
+import './style.css'
+
+//create App Vue
 const app = createApp(App)
 
-const options = {}
+//gunakan "Toast" di Vue Js dengan plugin "use"
+app.use(Toast)
 
-app.use(Toast, options)
 app.mount('#app')
